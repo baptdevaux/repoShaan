bc4c00877a689e23e7401e556e4007dc979fc5c4
theau <tpoixpro@gmail.com>
2019-11-13 15:33:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- createurShaan-data-jpa/src/test/java/test/TestJPA.java
+++ createurShaan-data-jpa/src/test/java/test/TestJPA.java
@@ -8,19 +8,115 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import createurShaan.Application;
+import createurShaan.model.AlterationEtat;
+import createurShaan.model.Caste;
+import createurShaan.model.DomainePersonnage;
+import createurShaan.model.Metier;
+import createurShaan.model.Personnage;
+import createurShaan.model.Peuple;
+import createurShaan.model.Race;
+import createurShaan.model.TableDeJeu;
+import createurShaan.repository.IAlterationEtatRepository;
+import createurShaan.repository.ICasteRepository;
+import createurShaan.repository.IDomainePersonnageRepository;
+import createurShaan.repository.IMetierRepository;
+import createurShaan.repository.IPersonnageRepository;
+import createurShaan.repository.IPeupleRepository;
+import createurShaan.repository.IRaceRepository;
+import createurShaan.repository.ITableDeJeuRepository;
+
 
 
 
 public class TestJPA {
 	public static void main(String[] args) throws ParseException {
 
+	EntityManagerFactory emf = Application.getInstance().getEmf();
 
-
-
+	
+	Personnage theau = new Personnage();
+	
+	IPersonnageRepository personnageRepository = Application.getInstance().getPersonnageRepo();
+	
+	
+	
+	
+	Peuple sable = new Peuple();
+	
+	IPeupleRepository peupleRepository = Application.getInstance().getPeupleRepo();
+	
+	sable.setNomPeuple("Sable");
+	
+	sable = peupleRepository.save(sable);
+	
+	
+	
+	Race delhion = new Race();
+	
+	IRaceRepository raceRepository = Application.getInstance().getRaceRepo();
+	
+	delhion.setCouleurSymbolique("gris");
+	delhion.setElementTotem("nature");
+	delhion.setLangues("la langue de chez les Delhion");
+	delhion.setNomRace("Delhion");
+	
+	delhion = raceRepository.save(delhion);
+	
+	
+	
+	
+	Metier prestidigitateur = new Metier();
+	
+	IMetierRepository metierRepository = Application.getInstance().getMetierRepo();			
+	
+	prestidigitateur.setNomMetier("Développeur Java");
+	prestidigitateur.setVoie("voie ferrée");
+	
+	prestidigitateur = metierRepository.save(prestidigitateur);
+	
+	
+	
+	Caste artiste = new Caste();
+	
+	ICasteRepository casteRepository = Application.getInstance().getCasteRepo();
+	
+	artiste.setNomCaste("artiste");
+	
+	artiste = casteRepository.save(artiste);
+	
+	
+	
+	AlterationEtat aveugle = new AlterationEtat();
+	
+	IAlterationEtatRepository alterationEtatRepository = Application.getInstance().getAlterationEtatRepo();
+	
+	aveugle.setNom("aveuglé");
+	
+	aveugle = alterationEtatRepository.save(aveugle);
+	
+	
+	
+	TableDeJeu parapleteam = new TableDeJeu();
+	
+	ITableDeJeuRepository tableDeJeuRepository = Application.getInstance().getTableDeJeuRepo();
+	
+	parapleteam.setNotes("Il y a du gateau et des meufs dans le frigo. Oups");
+	
+	parapleteam = tableDeJeuRepository.save(parapleteam);
+	
+	
+	
+	DomainePersonnage combatTheau = new DomainePersonnage();
+	
+	IDomainePersonnageRepository domainePersonnageRepository = Application.getInstance().getDomainePersonnageRepo();
+	
+	combatTheau.setRangDomaine(5);
+	
+	combatTheau = domainePersonnageRepository.save(combatTheau);
 	
 	
 	
 	
 	
-
-}}
+	}}
