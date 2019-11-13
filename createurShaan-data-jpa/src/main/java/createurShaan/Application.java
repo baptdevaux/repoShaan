cdeb2bb82165ee9ac9ad8e57500b624f6c0aa377
armand <armandpeyrelongue@gmail.com>
2019-11-13 13:17:32 +0100

    creation de l'application

--- createurShaan-data-jpa/src/main/java/createurShaan/Application.java
+++ createurShaan-data-jpa/src/main/java/createurShaan/Application.java
@@ -1,5 +1,26 @@
 package createurShaan;
 
-public class Application {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+
+public class Application {
+	private static Application instance = null;
+	
+	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("createurShaan");
+	
+	private Application() {
+	}
+	
+	public static Application getInstance() {
+		if (instance == null) {
+			instance = new Application();
+		}
+
+		return instance;
+	}
+
+	public EntityManagerFactory getEmf() {
+		return emf;
+	}
 }
