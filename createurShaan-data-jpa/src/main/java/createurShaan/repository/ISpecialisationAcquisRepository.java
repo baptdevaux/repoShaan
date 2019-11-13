11532191945c565a4d034a5334a99a855bee1859
armand <armandpeyrelongue@gmail.com>
2019-11-13 15:32:08 +0100

    Repo acquisSpe

---
+++ createurShaan-data-jpa/src/main/java/createurShaan/repository/ISpecialisationAcquisRepository.java
@@ -0,0 +1,6 @@
+package createurShaan.repository;
+
+import createurShaan.model.SpecialisationAcquis
+public interface ISpecialisationAcquisRepository extends IRepository<SpecialisationAcquis, Long>{
+
+}
