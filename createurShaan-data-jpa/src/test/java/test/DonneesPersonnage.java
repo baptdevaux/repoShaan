31bef46bb99d404f6e8b29730816b32a9b13ac07
Sylvain <sylh43@hotmail.fr>
2019-11-14 13:33:13 +0100

    Peuples

--- createurShaan-data-jpa/src/test/java/test/DonneesPersonnage.java
+++ createurShaan-data-jpa/src/test/java/test/DonneesPersonnage.java
@@ -94,23 +94,31 @@ public class DonneesPersonnage {
 //		CREATION OBJET PEUPLE
 //-------------------------------------------------------------------------------------------------------------------------
 		Peuple foretsBlanches = new Peuple();
-		Peuple sable = new Peuple();
+		Peuple glaces = new Peuple();
+		Peuple grandsarbres = new Peuple();
+		Peuple hautesHerbes = new Peuple();
+		Peuple marais = new Peuple();
+		Peuple montagnes = new Peuple();
+		Peuple rivages = new Peuple();
+		Peuple sables = new Peuple();
+		Peuple terresBurulees = new Peuple();
+		Peuple cites = new Peuple();
 
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET CASTE
 //-------------------------------------------------------------------------------------------------------------------------
-		Caste novateurs = new Caste();		
+		Caste novateurs = new Caste();
 		Caste erudits = new Caste();
 		Caste negociants = new Caste();
 		Caste artistes = new Caste();
 		Caste shaanistes = new Caste();
-		
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET METIER
 //-------------------------------------------------------------------------------------------------------------------------	
 
-		Metier batisseur =new Metier();
-		
+		Metier batisseur = new Metier();
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET MOTIVATION
 //-------------------------------------------------------------------------------------------------------------------------		
@@ -136,16 +144,18 @@ public class DonneesPersonnage {
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET SPECIALISATION
 //-------------------------------------------------------------------------------------------------------------------------	
-		engrenages.setDescription("Manipuler des petits mécanismes tels que serrures ou horloges. Poser et désarmocer des pièges complexes. Comprendre les systèmes à engrenages et poulies, mais également les machines à vapeur.");
+		engrenages.setDescription(
+				"Manipuler des petits mécanismes tels que serrures ou horloges. Poser et désarmocer des pièges complexes. Comprendre les systèmes à engrenages et poulies, mais également les machines à vapeur.");
 		engrenages.setNom("Engrenages");
 //		engrenages.setDomaineLie(technique);
 //		engrenages.setMetier(mecaniste);
 //		engrenages.getBonusAcquis().add(speAcquis);
-		engrenages=specialisationRepository.save(engrenages);
-		pilotage.setDescription("Conduire tout type de véhicule motorisé, du tank à la moto à réaction, en passant par l'aérojet, ou l'hydroglisseur. Connaître des rudiments techniques nécessaires à l'entretien et au bon fonctionnement du véhicule.");
+		engrenages = specialisationRepository.save(engrenages);
+		pilotage.setDescription(
+				"Conduire tout type de véhicule motorisé, du tank à la moto à réaction, en passant par l'aérojet, ou l'hydroglisseur. Connaître des rudiments techniques nécessaires à l'entretien et au bon fonctionnement du véhicule.");
 //		pilotage.setNom("Pilotage");
 //		pilotage.setDomaineLie(technique);
-	
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET RACE
 //-------------------------------------------------------------------------------------------------------------------------	
@@ -155,17 +165,90 @@ public class DonneesPersonnage {
 //-------------------------------------------------------------------------------------------------------------------------
 
 		foretsBlanches.setNomPeuple("Forêts Blanches");
-//		foretsBlanches.setDomaine(domaine);
-//		foretsBlanches.setSpecialisation1(specialisation1);
-//		foretsBlanches.setSpecialisation2(specialisation2);
+//		foretsBlanches.setDomaine(rituels);
+//		foretsBlanches.setSpecialisation1(langageDuCorps);
+//		foretsBlanches.setSpecialisation2(educationPhysique);
 		foretsBlanches.setDescription(
 				"De vastes forêts de conifères se recouvrent de neige l'hiver venu. La taïga, ou forêt boréale, étonne par sa densité et sa persistance à des altitudes défiant l'imagination. Les arbres sont moins grands que dans les régions chaudes mais les variétés de plantes sont tout aussi foisonnantes. De nombreux Temples rythment les haltes des Voyageurs.");
+		foretsBlanches = peupleRepository.save(foretsBlanches);
+
+		glaces.setNomPeuple("Glaces");
+//		glaces.setDomaine(magie);
+//		glaces.setSpecialisation1(arcanes);
+//		glaces.setSpecialisation2(langagePrimal);
+		glaces.setDescription(
+				"Les côtes sont bordées par une zone de glace aux contours instables. De nombreux plateaux recouverts de glaciers s'élèvent à plus de deux mille mètres. La vie au milieu des terres est impossible. Seul le littoral offre une réserve de gibier susceptible de nourrir les populations locales.");
+		glaces = peupleRepository.save(glaces);
+
+		grandsarbres.setNomPeuple("Grands Arbres");
+//		grandsarbres.setDomaine(shaan);
+//		grandsarbres.setSpecialisation1(empathieVegetale);
+//		grandsarbres.setSpecialisation2(empathieAnimale);
+		grandsarbres.setDescription(
+				"Cette épaisse forêt de gigantesques arbres verts peut atteindre une centaine de mètres de hauteur et son feuillage est persistant.");
+		grandsarbres = peupleRepository.save(grandsarbres);
+
+		hautesHerbes.setNomPeuple("Hautes Herbes");
+//		hautesHerbes.setDomaine(arts);
+//		hautesHerbes.setSpecialisation1(musique);
+//		hautesHerbes.setSpecialisation2(commerce);
+		hautesHerbes.setDescription(
+				"De vastes plaines d'herbe grasse s'étendent à l'infini. Parsemées d'arbres et de petits bosquets, ces régions sont principalement exploitées pour l'agriculture et l'élevage.");
+		hautesHerbes = peupleRepository.save(hautesHerbes);
+
+		marais.setNomPeuple("Hautes Herbes");
+//		marais.setDomaine(savoir);
+//		marais.setSpecialisation1(zoologie);
+//		marais.setSpecialisation2(botanique);
+		marais.setDescription(
+				"De hauts arbres plongent leurs puissantes racines dans les vastes étendues d'eau peu profondes des marais verts, jaunes et rouges. On ne trouve quasiment que des Ygwans dans les Marais, car eux seuls semblent apprécier la constante humidité et la chaleur de l'endroit.");
+		marais = peupleRepository.save(marais);
+
+		montagnes.setNomPeuple("Montagnes");
+//		montagnes.setDomaine(social);
+//		montagnes.setSpecialisation1(educationPhysique);
+//		montagnes.setSpecialisation2(languesExotiques);
+		montagnes.setDescription(
+				"De roches et de pics, cette région froide a été prise d'assaut par les Delhions. Des conifères parsèment le pied des Montagnes, pour rapidement laisser place à la roche puis à la neige. Les conditions de vie y sont rigoureuses, car l'oxygène se raréfie en haute altitude. La montagne est un environnement très dangereux pour celui qui ne la connait pas. L'.entre-aide reste donc une priorité pour ceux qui doivent survivre dans ce milieu. Habitués à cotoyer des natifs de tous horizons, les montagnards ont développé une facilité à communiquer dans d'autres langages que les leurs.");
+		montagnes = peupleRepository.save(montagnes);
+
+		rivages.setNomPeuple("Rivages");
+//		rivages.setDomaine(survie);
+//		rivages.setSpecialisation1(navigation);
+//		rivages.setSpecialisation2(educationPhysique);
+		rivages.setDescription(
+				"Le milieu varie selon le climat de la région, allant de la plage de sable fin bordée d'arbres enchanteurs dans les régions chaudes aux gigantesques falaises abruptes des contrées plus froides. Sur Héos, les marées ont une amplitude colossale. Cela oblige les Peuples de la mer à consolider constamment les digues et barrages nécessaires à leur survie. Les Ygwans et les Kelwi ns de ces milieux maîtrisent la construction de ces édifices protecteurs et un maçon a droit au même respect qu'un grand navigateur. Le littoral et les fonds marins offrent une profusion de plantes et d' animaux divers. Les cités lacustres peuvent s'étendre sur des kilomètres le long des côtes et se prolongent dans la mer grâce aux ports.");
+		rivages = peupleRepository.save(rivages);
+
+		sables.setNomPeuple("Sables");
+//		sables.setDomaine(combat);
+//		sables.setSpecialisation1(monture);
+//		sables.setSpecialisation2(histoireDHeos);
+		sables.setDescription(
+				"Les Sables sont de vastes étendues arides de sable tassé ou en formation dunaire et de rochers plus ou moins saillants. Perpétuellement balayé par de terribles vents, le désert est une transition entre les Terres Brûlées et les Hautes Herbes chaudes. Selon la présence ou l'absence des pluies, il oscillera entre ces deux régions.");
+		sables = peupleRepository.save(sables);
+
+		terresBurulees.setNomPeuple("Terres Brûlées");
+//		terresBurulees.setDomaine(technique);
+//		terresBurulees.setSpecialisation1(navigation);
+//		terresBurulees.setSpecialisation2(recuperation);
+		terresBurulees.setDescription(
+				"De gigantesques canyons, des plateaux qui s'élèvent à l'infini, des gouffres et des ravins qui transforment la lumière en ténèbres, tel est le décor des Terres Brûlées. Les Peuples des Terres Brûlées sont obligés de compenser la dureté de leur environnement par de rastuce et des trouvailles techniques. Si personne ne veut habiter les Terres Brûlées, nombreux sont ceux qui rêvent d'en récupérer toutes les richesses ...");
+		terresBurulees = peupleRepository.save(sables);
+
+		cites.setNomPeuple("Cités");
+//		cites.setDomaine(necrose);
+//		cites.setSpecialisation1(cultureHumaine);
+//		cites.setSpecialisation2(vieUrbaine);
+		cites.setDescription("Cités tentaculaires de bâtiments aux formes improbables, dressant leurs tours de pierre et de verre en offrande au ciel, ou au contraire plongeant leurs souterrains (égouts et habitations troglodytes) dans les entrailles de la terre, les mégalopoles héossiennes regorgent de vie . . . et de mort. Car si la vie y est agréable et animée dans les beaux quartiers et dans les secteurs résidentiels, les bas-fonds restent de vrais coupe-gorges et constituent des enclaves où l'Ombre impose ses lois.");
+		cites = peupleRepository.save(cites);
 
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET CASTE
 //-------------------------------------------------------------------------------------------------------------------------
 		novateurs.setNomCaste("Les Novateurs");
-		novateurs.setDescription("Cette Caste rassemble tous ceux qui travaillent le cuir, le métal, la pierre, le bois, le verre, le tissu, ainsi que les machinistes et ceux qui peuvent être amenés à utiliser de la technologie humaine. Cette Caste très influente dispose de nombreuses ramifications au sein des sociétés humaines et héossiennes grâce aux nombreux Ateliers de production qu'elle gère et qui constituent autant de points névralgiques de l'économie héossienne.");
+		novateurs.setDescription(
+				"Cette Caste rassemble tous ceux qui travaillent le cuir, le métal, la pierre, le bois, le verre, le tissu, ainsi que les machinistes et ceux qui peuvent être amenés à utiliser de la technologie humaine. Cette Caste très influente dispose de nombreuses ramifications au sein des sociétés humaines et héossiennes grâce aux nombreux Ateliers de production qu'elle gère et qui constituent autant de points névralgiques de l'économie héossienne.");
 //		novateurs.setDomaine(technique);
 //		novateurs.getMetiers().add(batisseur);
 //		novateurs.getMetiers().add(charpentier);
@@ -178,9 +261,10 @@ public class DonneesPersonnage {
 //		novateurs.getMetiers().add(recuperateur);
 //		novateurs.getMetiers().add(tanneur);
 		novateurs = casteRepository.save(novateurs);
-		
+
 		erudits.setNomCaste("Les Erudits");
-		erudits.setDescription("C'est l'élite intellectuelle de la population héossienne. Tous les scientifiques, philosophes, littéraires, ingénieurs et juristes y sont représentés. Le Nouvel Ordre et les Grandes Familles se méfient toujours des Érudits, qui ont été les premiers à comprendre la culture technologique humaine.");
+		erudits.setDescription(
+				"C'est l'élite intellectuelle de la population héossienne. Tous les scientifiques, philosophes, littéraires, ingénieurs et juristes y sont représentés. Le Nouvel Ordre et les Grandes Familles se méfient toujours des Érudits, qui ont été les premiers à comprendre la culture technologique humaine.");
 //		erudits.setDomaine(savoir);
 //		erudits.getMetiers().add(alchimiste);
 //		erudits.getMetiers().add(archiviste);
@@ -193,9 +277,10 @@ public class DonneesPersonnage {
 //		erudits.getMetiers().add(percepteur);
 //		erudits.getMetiers().add(zoologue);
 		erudits = casteRepository.save(erudits);
-		
+
 		negociants.setNomCaste("Les Négociants");
-		negociants.setDescription("La Caste des Négociants accueille tous ceux qui tissent des liens sociaux, que ce soit à but lucratif ou par intérêt politique. Cette Caste constitue la pierre angulaire de l'économie héossienne, mais est surtout un trait d' union avec les civilisations humaines et nécrosiennes. Le Négociant sait toujours tirer profit d'une situation, quelle qu'elle soit. Peu importe son interlocuteur, il sait que chacun dispose d'une faille qu'il suffit d'exploiter pour prendre l'avantage.");
+		negociants.setDescription(
+				"La Caste des Négociants accueille tous ceux qui tissent des liens sociaux, que ce soit à but lucratif ou par intérêt politique. Cette Caste constitue la pierre angulaire de l'économie héossienne, mais est surtout un trait d' union avec les civilisations humaines et nécrosiennes. Le Négociant sait toujours tirer profit d'une situation, quelle qu'elle soit. Peu importe son interlocuteur, il sait que chacun dispose d'une faille qu'il suffit d'exploiter pour prendre l'avantage.");
 //		negociants.setDomaine(social);
 //		negociants.getMetiers().add(courtisan);
 //		negociants.getMetiers().add(cyberien);
@@ -210,7 +295,8 @@ public class DonneesPersonnage {
 		negociants = casteRepository.save(negociants);
 
 		artistes.setNomCaste("Les Artistes");
-		artistes.setDescription("La Caste des Artistes rassemble tous ceux dont le but est de divertir la population, que ce soit par la musique, la danse, la comédie, la peinture, la gastronomie, la jonglerie ou les acrobaties. Le Nouvel Ordre n'aimait pas les Artistes, réprimant immédiatement toute manifestation publique. Avec la Révolution et la chute du régime humain, les choses ont changé et les Artistes sont aujourd'hui encouragés à créer, à exposer et à s'exprimer pour revaloriser la culture héossienne. Certains courants artistiques engendrent d'ailleurs de véritables effets de modes qui, grâce à Arpège, se diffusent à l'ensemble du continent, offrant aux Artistes une renommée, une notoriété et une popularité parfois plus importante que celle de toutes les autres Castes.");
+		artistes.setDescription(
+				"La Caste des Artistes rassemble tous ceux dont le but est de divertir la population, que ce soit par la musique, la danse, la comédie, la peinture, la gastronomie, la jonglerie ou les acrobaties. Le Nouvel Ordre n'aimait pas les Artistes, réprimant immédiatement toute manifestation publique. Avec la Révolution et la chute du régime humain, les choses ont changé et les Artistes sont aujourd'hui encouragés à créer, à exposer et à s'exprimer pour revaloriser la culture héossienne. Certains courants artistiques engendrent d'ailleurs de véritables effets de modes qui, grâce à Arpège, se diffusent à l'ensemble du continent, offrant aux Artistes une renommée, une notoriété et une popularité parfois plus importante que celle de toutes les autres Castes.");
 //		artistes.setDomaine(arts);
 //		artistes.getMetiers().add(bateleur);
 //		artistes.getMetiers().add(evocateur);
@@ -225,7 +311,8 @@ public class DonneesPersonnage {
 		artistes = casteRepository.save(artistes);
 
 		shaanistes.setNomCaste("Les Shaanistes");
-		shaanistes.setDescription("Les Shaanistes sont des porteurs de lumière. Transmettant le Shaan à qui l'accepte, ils ont pour charge d'apaiser les Âmes, d'harmoniser les Esprits et de réguler toute sorte de fanatisme. Le Nouvel Ordre a systématiquement persécuté les Shaanistes. Les génocides qui se sont produits ont, pendant un moment, rendu la pratique du Shaan dangereuse, mais aucun de ses adeptes ne s'est détourné de sa voie.");
+		shaanistes.setDescription(
+				"Les Shaanistes sont des porteurs de lumière. Transmettant le Shaan à qui l'accepte, ils ont pour charge d'apaiser les Âmes, d'harmoniser les Esprits et de réguler toute sorte de fanatisme. Le Nouvel Ordre a systématiquement persécuté les Shaanistes. Les génocides qui se sont produits ont, pendant un moment, rendu la pratique du Shaan dangereuse, mais aucun de ses adeptes ne s'est détourné de sa voie.");
 //		shaanistes.setDomaine(shaan);
 //		shaanistes.getMetiers().add(ameVive);
 //		shaanistes.getMetiers().add(chasseurDeSteles);
@@ -245,8 +332,9 @@ public class DonneesPersonnage {
 		batisseur.setNomMetier("Batisseur");
 //		batisseur.setVoie(voie);
 //		batisseur.setSpecialisation(specialisation);
-		batisseur.setDescription("Les Bâtisseurs savent construire des ponts, des maisons, des routes, tailler la pierre et préparer la Trihnite afin qu'elle soit enchantée. Irascibles aux mains cagneuses, ils passent leur temps à râler car rien ne se passe jamais comme ils avaient prévu. Il faut dire que l'exigence des Bâtisseurs est légendaire, car ils restent conscients que la plus petite erreur de calcul, ou le moindre défaut de construction peut être à l'origine de terribles accidents et causer la mort... Ce sens des responsabilités leur met une pression permanente qu'ils entretiennent, même s'ils n'ont pas de chantier à gérer. Mais c'est ainsi, les Bâtisseurs se sentent responsables des autres...");
-		
+		batisseur.setDescription(
+				"Les Bâtisseurs savent construire des ponts, des maisons, des routes, tailler la pierre et préparer la Trihnite afin qu'elle soit enchantée. Irascibles aux mains cagneuses, ils passent leur temps à râler car rien ne se passe jamais comme ils avaient prévu. Il faut dire que l'exigence des Bâtisseurs est légendaire, car ils restent conscients que la plus petite erreur de calcul, ou le moindre défaut de construction peut être à l'origine de terribles accidents et causer la mort... Ce sens des responsabilités leur met une pression permanente qu'ils entretiennent, même s'ils n'ont pas de chantier à gérer. Mais c'est ainsi, les Bâtisseurs se sentent responsables des autres...");
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET MOTIVATION
 //-------------------------------------------------------------------------------------------------------------------------		
