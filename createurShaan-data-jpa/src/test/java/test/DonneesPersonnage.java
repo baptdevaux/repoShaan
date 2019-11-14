dca8c8f386552a44e1fbedcc22e0cd66b13c4951
Baptiste <baptiste.devaux.1993@gmail.com>
2019-11-14 13:16:10 +0100

    Domaine sans liens

--- createurShaan-data-jpa/src/test/java/test/DonneesPersonnage.java
+++ createurShaan-data-jpa/src/test/java/test/DonneesPersonnage.java
@@ -78,7 +78,15 @@ public class DonneesPersonnage {
 //		CREATION OBJET DOMAINE
 //-------------------------------------------------------------------------------------------------------------------------
 		Domaine technique = new Domaine();
-		Domaine Savoir = new Domaine();
+		Domaine savoir = new Domaine();
+		Domaine social = new Domaine();
+		Domaine arts = new Domaine();
+		Domaine shaan = new Domaine();
+		Domaine magie = new Domaine();
+		Domaine rituels = new Domaine();
+		Domaine survie = new Domaine();
+		Domaine combat = new Domaine();
+		Domaine necrose = new Domaine();
 
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET SPECIALISATION
@@ -89,7 +97,16 @@ public class DonneesPersonnage {
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET RACE
 //-------------------------------------------------------------------------------------------------------------------------	
-
+		Race boreals = new Race();
+		Race darkens = new Race();
+		Race delhions = new Race();
+		Race felings = new Race();
+		Race kelwins = new Race();
+		Race melodiens = new Race();
+		Race nomois = new Race();
+		Race woons = new Race();
+		Race ygwans = new Race();
+		Race humains = new Race();
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET PEUPLE
 //-------------------------------------------------------------------------------------------------------------------------
@@ -99,18 +116,18 @@ public class DonneesPersonnage {
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET CASTE
 //-------------------------------------------------------------------------------------------------------------------------
-		Caste novateurs = new Caste();		
+		Caste novateurs = new Caste();
 		Caste erudits = new Caste();
 		Caste negociants = new Caste();
 		Caste artistes = new Caste();
 		Caste shaanistes = new Caste();
-		
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET METIER
 //-------------------------------------------------------------------------------------------------------------------------	
 
-		Metier batisseur =new Metier();
-		
+		Metier batisseur = new Metier();
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		CREATION OBJET MOTIVATION
 //-------------------------------------------------------------------------------------------------------------------------		
@@ -130,22 +147,118 @@ public class DonneesPersonnage {
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET DOMAINE
 //-------------------------------------------------------------------------------------------------------------------------
-		technique.setDescription("traduit le savoir-faire");
 		technique.setNom("Technique");
 		technique.setElementLie("Objet");
+		technique.setDescription(
+				"traduit le savoir-faire, l'intelligence de la main et la maitrise de la matière. Monter, démonter pour réinventer et à nouveau construire.");
+//		technique.getTypeMotivation().add(e);
+//		technique.getListeSpe().add(e);
+//		technique.getDomainePerso().add(e);
+//		technique.getListePouvoir().add(e);
+		technique = domaineRepository.save(technique);
+
+		savoir.setNom("Savoir");
+		savoir.setElementLie("Végétal");
+		savoir.setDescription("Evoque la soif de connaissance, la mémoire, l'intelect et l'analyse");
+//		savoir.getTypeMotivation().add(e);
+//		savoir.getListeSpe().add(e);
+//		savoir.getDomainePerso().add(e);
+//		savoir.getListePouvoir().add(e);
+		savoir = domaineRepository.save(savoir);
+
+		social.setNom("Social");
+		social.setElementLie("Air");
+		social.setDescription(
+				"Reflète le relationnel avec les autres Anthéens, l'éloquence, l'art d'articuler un discours, la séduction");
+//		social.getTypeMotivation().add(e);
+//		social.getListeSpe().add(e);
+//		social.getDomainePerso().add(e);
+//		social.getListePouvoir().add(e);
+		social = domaineRepository.save(social);
+
+		arts.setNom("Arts");
+		arts.setElementLie("Autre");
+		arts.setDescription("Exprime la firme artistique, la sensualité et la créativité");
+//		arts.getTypeMotivation().add(e);
+//		arts.getListeSpe().add(e);
+//		arts.getDomainePerso().add(e);
+//		arts.getListePouvoir().add(e);
+		arts = domaineRepository.save(arts);
+
+		shaan.setNom("Savoir");
+		shaan.setElementLie("Moi");
+		shaan.setDescription(
+				"Symbolise l'harmonie avec l'environnement, la magnétisme, la chance brute et la lutte contre les LImbes pour le règne de la paix et de l'équilibre");
+//		shaan.getTypeMotivation().add(e);
+//		shaan.getListeSpe().add(e);
+//		shaan.getDomainePerso().add(e);
+//		shaan.getListePouvoir().add(e);
+		shaan = domaineRepository.save(shaan);
+
+		magie.setNom("Magie");
+		magie.setElementLie("Végétal");
+		magie.setDescription(
+				"Incarne les facultés magiques qui permettent de déceler et contrôler les flux d'énergies Trihnique.");
+//		magie.getTypeMotivation().add(e);
+//		magie.getListeSpe().add(e);
+//		magie.getDomainePerso().add(e);
+//		magie.getListePouvoir().add(e);
+		magie = domaineRepository.save(magie);
+
+		rituels.setNom("Rituels");
+		rituels.setElementLie("Animal");
+		rituels.setDescription(
+				"Représente la foi dans les Eléments et la capacité à manipuler leur énergie primale pour s'approprier leurs facultés.");
+//		rituels.getTypeMotivation().add(e);
+//		rituels.getListeSpe().add(e);
+//		rituels.getDomainePerso().add(e);
+//		rituels.getListePouvoir().add(e);
+		rituels = domaineRepository.save(rituels);
+
+		survie.setNom("Survie");
+		survie.setElementLie("Eau");
+		survie.setDescription(
+				"Réunit les capacités de déplacement, de perception et d'adaptation à un environnement naturel.");
+//		technique.getTypeMotivation().add(e);
+//		technique.getListeSpe().add(e);
+//		technique.getDomainePerso().add(e);
+//		technique.getListePouvoir().add(e);
+		survie = domaineRepository.save(survie);
+
+		combat.setNom("Combat");
+		combat.setElementLie("Feu");
+		combat.setDescription(
+				"Utilisé dans toute confrontation physique, tant qu'il s'agit de manier une arme non-humaine.");
+//		technique.getTypeMotivation().add(e);
+//		technique.getListeSpe().add(e);
+//		technique.getDomainePerso().add(e);
+//		technique.getListePouvoir().add(e);
+		combat = domaineRepository.save(combat);
+
+		necrose.setNom("Necrose");
+		necrose.setElementLie("Limbes");
+		necrose.setDescription(
+				"Personnifie le côté sombre, le chamin parcouru par le personnage sur la voie des Limbes. Il sert à faire beaucoup d'actions peu recommandable et à mesurer son degré de décomposition et l'accumulations de pulsions incontrôlables.");
+//		technique.getTypeMotivation().add(e);
+//		technique.getListeSpe().add(e);
+//		technique.getDomainePerso().add(e);
+//		technique.getListePouvoir().add(e);
+		necrose = domaineRepository.save(necrose);
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET SPECIALISATION
 //-------------------------------------------------------------------------------------------------------------------------	
-		engrenages.setDescription("Manipuler des petits mécanismes tels que serrures ou horloges. Poser et désarmocer des pièges complexes. Comprendre les systèmes à engrenages et poulies, mais également les machines à vapeur.");
+		engrenages.setDescription(
+				"Manipuler des petits mécanismes tels que serrures ou horloges. Poser et désarmocer des pièges complexes. Comprendre les systèmes à engrenages et poulies, mais également les machines à vapeur.");
 		engrenages.setNom("Engrenages");
 //		engrenages.setDomaineLie(technique);
 //		engrenages.setMetier(mecaniste);
 //		engrenages.getBonusAcquis().add(speAcquis);
-		engrenages=specialisationRepository.save(engrenages);
-		pilotage.setDescription("Conduire tout type de véhicule motorisé, du tank à la moto à réaction, en passant par l'aérojet, ou l'hydroglisseur. Connaître des rudiments techniques nécessaires à l'entretien et au bon fonctionnement du véhicule.");
+		engrenages = specialisationRepository.save(engrenages);
+		pilotage.setDescription(
+				"Conduire tout type de véhicule motorisé, du tank à la moto à réaction, en passant par l'aérojet, ou l'hydroglisseur. Connaître des rudiments techniques nécessaires à l'entretien et au bon fonctionnement du véhicule.");
 //		pilotage.setNom("Pilotage");
 //		pilotage.setDomaineLie(technique);
-	
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET RACE
 //-------------------------------------------------------------------------------------------------------------------------	
@@ -165,7 +278,8 @@ public class DonneesPersonnage {
 //		REMPLISSAGE OBJET CASTE
 //-------------------------------------------------------------------------------------------------------------------------
 		novateurs.setNomCaste("Les Novateurs");
-		novateurs.setDescription("Cette Caste rassemble tous ceux qui travaillent le cuir, le métal, la pierre, le bois, le verre, le tissu, ainsi que les machinistes et ceux qui peuvent être amenés à utiliser de la technologie humaine. Cette Caste très influente dispose de nombreuses ramifications au sein des sociétés humaines et héossiennes grâce aux nombreux Ateliers de production qu'elle gère et qui constituent autant de points névralgiques de l'économie héossienne.");
+		novateurs.setDescription(
+				"Cette Caste rassemble tous ceux qui travaillent le cuir, le métal, la pierre, le bois, le verre, le tissu, ainsi que les machinistes et ceux qui peuvent être amenés à utiliser de la technologie humaine. Cette Caste très influente dispose de nombreuses ramifications au sein des sociétés humaines et héossiennes grâce aux nombreux Ateliers de production qu'elle gère et qui constituent autant de points névralgiques de l'économie héossienne.");
 //		novateurs.setDomaine(technique);
 //		novateurs.getMetiers().add(batisseur);
 //		novateurs.getMetiers().add(charpentier);
@@ -178,9 +292,10 @@ public class DonneesPersonnage {
 //		novateurs.getMetiers().add(recuperateur);
 //		novateurs.getMetiers().add(tanneur);
 		novateurs = casteRepository.save(novateurs);
-		
+
 		erudits.setNomCaste("Les Erudits");
-		erudits.setDescription("C'est l'élite intellectuelle de la population héossienne. Tous les scientifiques, philosophes, littéraires, ingénieurs et juristes y sont représentés. Le Nouvel Ordre et les Grandes Familles se méfient toujours des Érudits, qui ont été les premiers à comprendre la culture technologique humaine.");
+		erudits.setDescription(
+				"C'est l'élite intellectuelle de la population héossienne. Tous les scientifiques, philosophes, littéraires, ingénieurs et juristes y sont représentés. Le Nouvel Ordre et les Grandes Familles se méfient toujours des Érudits, qui ont été les premiers à comprendre la culture technologique humaine.");
 //		erudits.setDomaine(savoir);
 //		erudits.getMetiers().add(alchimiste);
 //		erudits.getMetiers().add(archiviste);
@@ -193,9 +308,10 @@ public class DonneesPersonnage {
 //		erudits.getMetiers().add(percepteur);
 //		erudits.getMetiers().add(zoologue);
 		erudits = casteRepository.save(erudits);
-		
+
 		negociants.setNomCaste("Les Négociants");
-		negociants.setDescription("La Caste des Négociants accueille tous ceux qui tissent des liens sociaux, que ce soit à but lucratif ou par intérêt politique. Cette Caste constitue la pierre angulaire de l'économie héossienne, mais est surtout un trait d' union avec les civilisations humaines et nécrosiennes. Le Négociant sait toujours tirer profit d'une situation, quelle qu'elle soit. Peu importe son interlocuteur, il sait que chacun dispose d'une faille qu'il suffit d'exploiter pour prendre l'avantage.");
+		negociants.setDescription(
+				"La Caste des Négociants accueille tous ceux qui tissent des liens sociaux, que ce soit à but lucratif ou par intérêt politique. Cette Caste constitue la pierre angulaire de l'économie héossienne, mais est surtout un trait d' union avec les civilisations humaines et nécrosiennes. Le Négociant sait toujours tirer profit d'une situation, quelle qu'elle soit. Peu importe son interlocuteur, il sait que chacun dispose d'une faille qu'il suffit d'exploiter pour prendre l'avantage.");
 //		negociants.setDomaine(social);
 //		negociants.getMetiers().add(courtisan);
 //		negociants.getMetiers().add(cyberien);
@@ -210,7 +326,8 @@ public class DonneesPersonnage {
 		negociants = casteRepository.save(negociants);
 
 		artistes.setNomCaste("Les Artistes");
-		artistes.setDescription("La Caste des Artistes rassemble tous ceux dont le but est de divertir la population, que ce soit par la musique, la danse, la comédie, la peinture, la gastronomie, la jonglerie ou les acrobaties. Le Nouvel Ordre n'aimait pas les Artistes, réprimant immédiatement toute manifestation publique. Avec la Révolution et la chute du régime humain, les choses ont changé et les Artistes sont aujourd'hui encouragés à créer, à exposer et à s'exprimer pour revaloriser la culture héossienne. Certains courants artistiques engendrent d'ailleurs de véritables effets de modes qui, grâce à Arpège, se diffusent à l'ensemble du continent, offrant aux Artistes une renommée, une notoriété et une popularité parfois plus importante que celle de toutes les autres Castes.");
+		artistes.setDescription(
+				"La Caste des Artistes rassemble tous ceux dont le but est de divertir la population, que ce soit par la musique, la danse, la comédie, la peinture, la gastronomie, la jonglerie ou les acrobaties. Le Nouvel Ordre n'aimait pas les Artistes, réprimant immédiatement toute manifestation publique. Avec la Révolution et la chute du régime humain, les choses ont changé et les Artistes sont aujourd'hui encouragés à créer, à exposer et à s'exprimer pour revaloriser la culture héossienne. Certains courants artistiques engendrent d'ailleurs de véritables effets de modes qui, grâce à Arpège, se diffusent à l'ensemble du continent, offrant aux Artistes une renommée, une notoriété et une popularité parfois plus importante que celle de toutes les autres Castes.");
 //		artistes.setDomaine(arts);
 //		artistes.getMetiers().add(bateleur);
 //		artistes.getMetiers().add(evocateur);
@@ -225,7 +342,8 @@ public class DonneesPersonnage {
 		artistes = casteRepository.save(artistes);
 
 		shaanistes.setNomCaste("Les Shaanistes");
-		shaanistes.setDescription("Les Shaanistes sont des porteurs de lumière. Transmettant le Shaan à qui l'accepte, ils ont pour charge d'apaiser les Âmes, d'harmoniser les Esprits et de réguler toute sorte de fanatisme. Le Nouvel Ordre a systématiquement persécuté les Shaanistes. Les génocides qui se sont produits ont, pendant un moment, rendu la pratique du Shaan dangereuse, mais aucun de ses adeptes ne s'est détourné de sa voie.");
+		shaanistes.setDescription(
+				"Les Shaanistes sont des porteurs de lumière. Transmettant le Shaan à qui l'accepte, ils ont pour charge d'apaiser les Âmes, d'harmoniser les Esprits et de réguler toute sorte de fanatisme. Le Nouvel Ordre a systématiquement persécuté les Shaanistes. Les génocides qui se sont produits ont, pendant un moment, rendu la pratique du Shaan dangereuse, mais aucun de ses adeptes ne s'est détourné de sa voie.");
 //		shaanistes.setDomaine(shaan);
 //		shaanistes.getMetiers().add(ameVive);
 //		shaanistes.getMetiers().add(chasseurDeSteles);
@@ -245,8 +363,9 @@ public class DonneesPersonnage {
 		batisseur.setNomMetier("Batisseur");
 //		batisseur.setVoie(voie);
 //		batisseur.setSpecialisation(specialisation);
-		batisseur.setDescription("Les Bâtisseurs savent construire des ponts, des maisons, des routes, tailler la pierre et préparer la Trihnite afin qu'elle soit enchantée. Irascibles aux mains cagneuses, ils passent leur temps à râler car rien ne se passe jamais comme ils avaient prévu. Il faut dire que l'exigence des Bâtisseurs est légendaire, car ils restent conscients que la plus petite erreur de calcul, ou le moindre défaut de construction peut être à l'origine de terribles accidents et causer la mort... Ce sens des responsabilités leur met une pression permanente qu'ils entretiennent, même s'ils n'ont pas de chantier à gérer. Mais c'est ainsi, les Bâtisseurs se sentent responsables des autres...");
-		
+		batisseur.setDescription(
+				"Les Bâtisseurs savent construire des ponts, des maisons, des routes, tailler la pierre et préparer la Trihnite afin qu'elle soit enchantée. Irascibles aux mains cagneuses, ils passent leur temps à râler car rien ne se passe jamais comme ils avaient prévu. Il faut dire que l'exigence des Bâtisseurs est légendaire, car ils restent conscients que la plus petite erreur de calcul, ou le moindre défaut de construction peut être à l'origine de terribles accidents et causer la mort... Ce sens des responsabilités leur met une pression permanente qu'ils entretiennent, même s'ils n'ont pas de chantier à gérer. Mais c'est ainsi, les Bâtisseurs se sentent responsables des autres...");
+
 //-------------------------------------------------------------------------------------------------------------------------
 //		REMPLISSAGE OBJET MOTIVATION
 //-------------------------------------------------------------------------------------------------------------------------		
